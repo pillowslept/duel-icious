3346239f757549181b9690fdd71783ab5a37d385
Juan Camilo Velásquez Vanegas <juancamilo---142@hotmail.com>
2020-05-23 19:53:17 -0500

    Initial project configuration.

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Characters } from './views/Characters';
+import { Specialities } from './views/Specialities';
+import { Duels } from './views/Duels';
+import { Home } from './views/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <div className="main-container">
+          <Switch>
+            <Route path="/characters">
+              <Characters />
+            </Route>
+            <Route path="/specialities">
+              <Specialities />
+            </Route>
+            <Route path="/duels">
+              <Duels />
+            </Route>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/actions/index.js
@@ -0,0 +1,49 @@
+// CHARACTERS
+
+export const addCharacter = (character) => {
+  return {
+    type: 'ADD_CHARACTER',
+    payload: character,
+  }
+}
+
+export const updateCharacter = (character) => {
+  return {
+    type: 'UPDATE_CHARACTER',
+    payload: character,
+  }
+}
+
+// DUELS
+
+export const startDuel = (leftCharacter, rightCharacter) => {
+  return {
+    type: 'START_DUEL',
+    payload: {
+      leftCharacter,
+      rightCharacter
+    }
+  }
+}
+
+export const finishDuel = () => {
+  return {
+    type: 'FINISH_DUEL',
+  }
+}
+
+export const addDuelWinner = (duel) => {
+  return {
+    type: 'ADD_DUEL_WINNER',
+    payload: duel
+  }
+}
+
+// SPECIALITIES
+
+export const addSpeciality = (speciality) => {
+  return {
+    type: 'ADD_SPECIALITY',
+    payload: speciality
+  }
+}
